5782920d299e4aafd24c0171473a5b9aacc86b6c
Ruslan Pilin <dohque@gmail.com>
2016-12-10 18:15:04 -0500

    displaying sate with red and green leds

--- displayState.js
+++ displayState.js
@@ -1,6 +1,23 @@
+var mraa = require('mraa');
 
 function displayState(env) {
   console.log("displayState: " + JSON.stringify(env));
+  var redLed = new mraa.Gpio(5);
+  var greenLed = new mraa.Gpio(6);
+  if (env.isGood) {
+    redLed.dir(mraa.DIR_OUT);
+    redLed.write(0);
+
+    greenLed.dir(mraa.DIR_OUT);
+    greenLed.write(1);
+  }
+  if (!env.isGood) {
+    redLed.dir(mraa.DIR_OUT);
+    redLed.write(1);
+
+    greenLed.dir(mraa.DIR_OUT);
+    greenLed.write(0);
+  }
 }
 
 module.exports = displayState;
