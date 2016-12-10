f7d7c9ea651009e325627688e682815363dca1bd
David Kim <davidkim@rrcs-198-179-74-14.nyc.biz.rr.com>
2016-12-10 16:32:39 -0500

    Testing moisture

--- test.js
+++ test.js
@@ -1,8 +1,16 @@
 var mraa = require('mraa');
 var led = new mraa.Gpio(6);
 led.dir(mraa.DIR_OUT);
-var toggle = false;
+
+// Light toggle example
+// var toggle = false;
+// setInterval(function(){
+//   toggle = !toggle;
+//   led.write(toggle?1:0); 
+// }, 1000);
+
+var moistureSensor = new mraa.Aio(0);
+var moistureValue = moistureSensor.read();
 setInterval(function(){
-  toggle = !toggle;
-  led.write(toggle?1:0); 
+  console.log("Moisture Value ", moistureValue);
 }, 1000);
