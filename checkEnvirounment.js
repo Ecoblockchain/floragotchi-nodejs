b38ac3d410049a154c7434cd240ed38b93271e40
Ruslan Pilin <dohque@gmail.com>
2016-12-11 10:00:36 -0500

    moisure sensor integrated

--- checkEnvirounment.js
+++ checkEnvirounment.js
@@ -3,15 +3,18 @@ var sensors = require('./sensors');
 
 var board = sensors.initBoard();
 var lightSensor = sensors.getAnalogSensor('A0');
-// var tempSensor = sensors. getAnalogSensor('A1');
+var moistureSensor = sensors.getAnalogSensor('A2');
+// var tempSensor = sensors.getAnalogSensor('A2');
 
 function checkEnvirounment() {
   var light = lightSensor.value;
-  var isGood = (light > 200);
+  var moisture = moistureSensor.value;
+  var isGood = (light > 200 && moisture > 200);
 
   return {
     "isGood": isGood,
     "light": light,
+    "moisture": moisture
     // "temp": tempSensor.value
   };
 }
